079890492472b3baba1c715dbfa75f45f96b0b0c
Nicholas Stienz <nickstienz@gmail.com>
2023-06-26 18:14:41 -0500

    lick

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
 fn main() {
-    println!("Hello, CRY-1!");
+    // This will be the assembler for the CRY-1 Computer so buckle up and enjoy your ride to hell!
+    println!("=== CRY-1 Assembler v0.1.0 by Nicholas Stienz ===");
+
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: cry-1 <file>");
+        std::process::exit(1);
+    }
+    let filename = &args[1];
+    println!("Assembling file: {}", filename);
+
+    let contents_result = std::fs::read_to_string(filename);
+    let contents = match contents_result {
+        Ok(contents) => contents,
+        Err(err) => match err.kind() {
+            std::io::ErrorKind::NotFound => {
+                println!("File not found: {}", filename);
+                std::process::exit(1);
+            }
+            std::io::ErrorKind::PermissionDenied => {
+                println!("Permission denied: {}", filename);
+                std::process::exit(1);
+            }
+            _ => {
+                println!("Error reading file: {}", filename);
+                std::process::exit(1);
+            }
+        },
+    };
+
+    println!("With text:\n{}", contents);
 }
---
+++ test.cry
@@ -0,0 +1,36 @@
+# This is a simple step by step process for me to reference right now!
+
+_start:
+    lda #15     ; Load 15 into A
+    ldb #5      ; Load 5 into B
+    add         ; Add A and B
+    output      ; Output A
+    halt        ; Halt
+
+---
+
+0x0: 0x00 #15
+0x1: 0x01 #5
+0x2: 0x03
+0x3: 0x08
+0x4: 0x06
+
+---
+
+0x0: 0x00 0x5
+0x1: 0x01 0x6
+0x2: 0x03  
+0x3: 0x08
+0x4: 0x06
+0x5: 0x0F
+0x6: 0x05
+
+---
+
+00000000: 00000000 00000101
+00000001: 00000001 00000110
+00000010: 00000011
+00000011: 00001000
+00000100: 00000110
+00000101: 00001111
+00000110: 00000101
