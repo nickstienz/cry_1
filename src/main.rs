fa4362e792ba216a1e97bdf5d03c95a1b3e3faf3
Nicholas Stienz <nickstienz@gmail.com>
2023-09-11 17:54:50 -0500

    This is somewhat fun :smile:

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,17 @@ mod token;
 use lexer::*;
 
 fn main() {
-    println!("=== CRY-1 Assembler by Nicholas Stienz ===");
-
     let args: Vec<String> = std::env::args().collect();
     if args.len() < 2 {
-        println!("Usage: cry_1 [filename] [option]");
+        println!("Usage: cry_1 [filename] [option]\nHelp: cry_1 help");
         std::process::exit(1);
     }
 
     let filename = &args[1];
+    if filename == "--help" || filename == "-h" || filename == "help" {
+        help();
+        std::process::exit(0);
+    }
     let contents = match std::fs::read_to_string(filename) {
         Ok(contents) => contents,
         Err(error) => {
@@ -25,13 +27,25 @@ fn main() {
     if args.len() > 2 {
         option = &args[2];
     }
+    if option == "--help" || filename == "-h" || filename == "help" {
+        help();
+        std::process::exit(0);
+    }
 
     let mut lexer = Lexer::new(contents);
 
-    if option == "--tokens" || option == "-t" {
+    if option == "--tokens" || option == "-t" || option == "tokens" {
         while let Some(token) = lexer.next() {
             println!("{:?}", token);
         }
-        return;
+        std::process::exit(0);
     }
+}
+
+fn help() {
+    println!("=== Cry_1 Assembler made by Nicholas Stienz ===");
+    println!("Usage: cry_1 [filename] [option]\n");
+    println!("Options:");
+    println!("Help:\t\t--help, -h, help\t\t- Prints this help message.");
+    println!("Tokens:\t\t--tokens, -t, tokens\t\t- Prints all the tokens found in the program.");
 }
